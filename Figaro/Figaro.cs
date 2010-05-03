3f3e06570d737eb8d49907040b06f81532e44cc5
msuarz <mikemps@gmail.com>
2010-05-02 21:28:59 -0400

    added basic http auth

--- Figaro/Figaro.cs
+++ Figaro/Figaro.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Net;
 using fit;
@@ -6,31 +7,55 @@ namespace Figaro {
     
     public class Figaro {
 
-        string RequestUriString { get { return "http://" + Host + "/" + Resource; } }
+        string RequestUriString { get { return 
+            string.IsNullOrEmpty(Host) ? URI : "http://" + Host + "/" + URI; 
+        }}
 
-        public void Get(string Resource) {
-            this.Resource = Resource;
+        public void Get(string URI) {
+            ClearFields();
+            this.URI = URI;
         }
 
-        string Resource { get; set; }
-        public string Host { get; set; }
+        void ClearFields() {
+            Host = Authorization = UserName = Password = null;
+        }
+
+        string URI { get; set; }
+
+        WebRequest Request { get; set; }
         WebResponse Response { get; set; }
 
+        public string Host { get; set; }
+        public string Authorization { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
         public void Send() {
-            Response = WebRequest.Create(RequestUriString).GetResponse();
+            Request = WebRequest.Create(RequestUriString);
+            AddAuthorization(); 
+            Response = Request.GetResponse();
         }
-        
+
+        void AddAuthorization() { 
+            if (string.IsNullOrEmpty(Authorization)) return;
+
+            var Token = Convert.ToBase64String(
+                System.Text.Encoding.UTF8.GetBytes(UserName + ":" + Password));
+            
+            Request.Headers.Add("Authorization", Authorization + " " + Token);
+        }
+
         public Fixture ResponseHeader {
             get { return new HeaderColumnFixture(Response.Headers); }
         }
 
         public Fixture ResponseBody { get {
-            var Fixtureixture = new ColumnFixture();
+            var Fixture = new ColumnFixture();
 
             using( var Reader = new StreamReader(Response.GetResponseStream()))
-                Fixtureixture.SetSystemUnderTest(new { Content = Reader.ReadToEnd() });
+                Fixture.SetSystemUnderTest(new { Content = Reader.ReadToEnd() });
 
-            return Fixtureixture;
+            return Fixture;
         }}
     }
 }
