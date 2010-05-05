6501d70c1c3dc15e5e475a8c0fb105e25d6d0a61
unknown <Ravi@.(none)>
2010-05-05 00:23:35 -0400

    Added JSONResult, XMLResult. Added Json.Net assembly to dot net folder. Modified Rate Limit Status test with JSON stuff. Need to refactor some so it doesn't keep parsing stuff. :)

---
+++ Figaro/JSONResult.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+namespace Figaro
+{
+    class JSONResult
+    {
+        public static JSONResult GetValueFor(string propertyName)
+        {
+            return new JSONResult(propertyName);
+        }
+
+        string PropertyName { get; set; }
+
+        private JSONResult(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+
+        public string ContainedIn(string rawJSON)
+        {
+            var result = string.Empty;
+            var jsonValues = JsonConvert.DeserializeObject<Dictionary<string, string>>(rawJSON);
+            return jsonValues[PropertyName];
+        }
+
+    }
+}
---
+++ Figaro/XMLResult.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Figaro
+{
+    class XMLResult
+    {
+
+        public static XMLResult GetValueFor(string xPath)
+        {
+            return new XMLResult(xPath);
+        }
+
+        string XPath { get; set; }
+
+        private XMLResult(string xPath)
+        {
+            XPath = xPath;
+        }
+
+        public string ContainedIn(string rawXml)
+        {
+            return XDocument.Parse(rawXml).XPathSelectElement(XPath).Value;
+        }
+
+    }
+}
