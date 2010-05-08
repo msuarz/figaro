0d6ee09500ca67d9ba0148af43c9aed0de024d0b
msuarz <mikemps@gmail.com>
2010-05-08 19:19:20 -0400

    added ruby restructured tests

---
+++ Tests/RSpecs/specs/when_making_an_Http_Request_spec.rb
@@ -0,0 +1,26 @@
+require "spec"
+require File.join(File.dirname(__FILE__), "../../Features/dotnet","Figaro.dll")
+
+describe Figaro::HttpFixture, "when making an Http Request" do
+
+  before(:each) do
+    @sut = Figaro::HttpFixture.new
+  end
+
+  it "should be able to GET" do
+
+    @sut.Get "localhost"
+    @sut.Method.should == "GET"
+    @sut.Uri.should == "localhost"
+
+  end
+
+  it "should not reuse fields between Requests" do
+
+    @sut.Host = "previous host"
+    @sut.Get "localhost"
+    @sut.Host.should be_nil
+
+  end
+
+end
