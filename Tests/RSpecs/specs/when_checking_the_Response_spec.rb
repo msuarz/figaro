8ca76fe9a811923cb0e61d0b295641f0c6822f4e
msuarz <mikemps@gmail.com>
2010-05-14 21:30:00 -0400

    done with Xml response test

--- Tests/RSpecs/specs/when_checking_the_Response_spec.rb
+++ Tests/RSpecs/specs/when_checking_the_Response_spec.rb
@@ -12,5 +12,15 @@ describe "when checking the Response" do
       @sut.Content =  "<field>42</field>"
       @sut.ValueOf("/field").should == "42"
     end
+
+    it "should be able to find Values using Object Path" do
+      @sut.Content =  "<dog><name>Fido</name></dog>"
+      @sut.ValueOf("dog.name").should == "Fido"
+    end
+
+    it "should be able to find Values with a Part Prefix" do
+      @sut.Content = "<person><address><city>Miami</city></address></person>"
+      @sut.ValueOf("city","person.address").should == "Miami"
+    end
   end
 end
